547b6c3d404a9bd4023050472eb91e0342c1f654
Brian Hong <sungjinhong@devsisters.com>
2014-11-18 01:37:40 +0900

    Fix docker proxy not working with this config

--- bootstrap-nginx.sh
+++ bootstrap-nginx.sh
@@ -24,31 +24,40 @@ then
 fi
 
 if [ -n "${SERVICE_NAME}" ]; then
-	eval "BACKEND_SERVICE_HOST=\$${SERVICE_NAME}_SERVICE_HOST"
-	eval "BACKEND_SERVICE_PORT=\$${SERVICE_NAME}_SERVICE_PORT"
+    eval "BACKEND_SERVICE_HOST=\$${SERVICE_NAME}_SERVICE_HOST"
+    eval "BACKEND_SERVICE_PORT=\$${SERVICE_NAME}_SERVICE_PORT"
 fi
 
 rm -rf /etc/nginx/sites-enabled/*
 cat <<EOF > /etc/nginx/sites-enabled/sslservice
 # HTTPS server
 server {
-	listen 443;
-	server_name localhost;
+    listen 443;
+    server_name localhost;
 
-	ssl on;
-	ssl_certificate /cert/cert.pem;
-	ssl_certificate_key /cert/cert.key;
+    # disable any limits to avoid HTTP 413 for large image uploads
+    client_max_body_size 0;
 
-	ssl_session_timeout 5m;
+    # required to avoid HTTP 411: see Issue #1486 (https://github.com/docker/docker/issues/1486)
+    chunked_transfer_encoding on;
 
-	ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-	ssl_ciphers "HIGH:!aNULL:!MD5 or HIGH:!aNULL:!MD5:!3DES";
-	ssl_prefer_server_ciphers on;
+    # Docker uses host header for proper URL redirects (https://github.com/docker/docker-registry/issues/69)
+    proxy_set_header Host \$http_host;
 
-	location / {
-		proxy_pass http://${BACKEND_SERVICE_HOST}:${BACKEND_SERVICE_PORT};
-	}
+    ssl on;
+    ssl_certificate /cert/cert.pem;
+    ssl_certificate_key /cert/cert.key;
 
-        access_log /dev/stdout;
+    ssl_session_timeout 5m;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers "HIGH:!aNULL:!MD5 or HIGH:!aNULL:!MD5:!3DES";
+    ssl_prefer_server_ciphers on;
+
+    location / {
+        proxy_pass http://${BACKEND_SERVICE_HOST}:${BACKEND_SERVICE_PORT};
+    }
+
+    access_log /dev/stdout;
 }
 EOF
